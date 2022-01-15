a8ef38b1f31421a220b886358e0555263bcf36e0
sadiet20 <sadierosethomas@gmail.com>
2022-01-15 13:49:32 -0800

    added colors

--- run_files.sh
+++ run_files.sh
@@ -2,8 +2,13 @@
 Author: Sadie Thomas
 Date: 1/15/2022
 Description: Runs executable on input files and checks results against answer file
-Instructions: Compile code to executable named ./a.out then run $time bash run_files.sh
+			 Diff prints out the correct answer then your solution
+Instructions: Compile code to executable named ./a.out then run $time bash run_tests.sh
 comment
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NOCOLOR='\033[0m'
 
 #runs against all .in and .ans files in the ./data/sample directory (change path if necessary)
 for f in ./data/sample/*.in ; 
@@ -12,9 +17,9 @@ for f in ./data/sample/*.in ;
 		rv=$?
 		if [[ $rv == 1 ]]
 		then 
-			echo "Failed test $f"
+			echo -e "${RED}Failed test $f ${NOCOLOR} \n"
 		else
-			echo "Passed test $f"
+			echo -e "${GREEN}Passed test $f ${NOCOLOR}"
 		fi
 	done
 
@@ -25,8 +30,8 @@ for f in ./data/secret/*.in ;
 		rv=$?
 		if [[ $rv == 1 ]]
 		then 
-			echo "Failed test $f"
+			echo -e "${RED}Failed test $f ${NOCOLOR} \n"
 		else
-			echo "Passed test $f"
+			echo -e "${GREEN}Passed test $f ${NOCOLOR}"
 		fi
 	done
